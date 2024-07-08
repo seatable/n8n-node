ed90fafc92f231e6d47684be751f229c71e5f269
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2024-07-08 07:41:41 +0200

    final version for merge Jun27th

--- nodes/SeaTable/v1/VersionDescription.ts
+++ nodes/SeaTable/v1/VersionDescription.ts
@@ -1,15 +1,13 @@
+/* eslint-disable n8n-nodes-base/node-filename-against-convention */
 import type { INodeTypeDescription } from 'n8n-workflow';
-import * as row from './row';
-import * as base from './base';
-import * as link from './link';
-import * as asset from './asset';
+import { rowFields, rowOperations } from './RowDescription';
 
 export const versionDescription: INodeTypeDescription = {
 	displayName: 'SeaTable',
 	name: 'seaTable',
 	icon: 'file:seaTable.svg',
-	group: ['output'],
-	version: 2,
+	group: ['input'],
+	version: 1,
 	subtitle: '={{$parameter["resource"] + ": " + $parameter["operation"]}}',
 	description: 'Consume the SeaTable API',
 	defaults: {
@@ -34,24 +32,10 @@ export const versionDescription: INodeTypeDescription = {
 					name: 'Row',
 					value: 'row',
 				},
-				{
-					name: 'Base',
-					value: 'base',
-				},
-				{
-					name: 'Link',
-					value: 'link',
-				},
-				{
-					name: 'Asset',
-					value: 'asset',
-				},
 			],
 			default: 'row',
 		},
-		...row.descriptions,
-		...base.descriptions,
-		...link.descriptions,
-		...asset.descriptions,
+		...rowOperations,
+		...rowFields,
 	],
 };
